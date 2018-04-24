1b30ac6caad5e908b07b3bbbfea4ba48737ce17a
noorsami <nordinsamibouchrit@gmail.com>
2018-04-24 10:42:05 +0200

    add swapcount

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import random as rm
 import math as m
 import sys
@@ -9,14 +8,14 @@ mir = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21
 # define swap function
 def swapMel(a, b, mel):
 	mel[a:b + 1] = mel[a:b + 1][::-1]
-
+	return mel
 
 # random generator
 def randomGen(mel, mir):
 
 	# run until mel and mir are the same
 	#while mel != mir:
-   		
+
 	# generate random numbers
 	a = rm.randint(0,24)
 	b = rm.randint(0,24)
@@ -24,30 +23,36 @@ def randomGen(mel, mir):
 
 	if a > b:
 		b, a = a, b
-		
-	print mel[a] 
-	print mel[b]
-	print mel
-		
+
+	print(mel[a])
+	print(mel[b])
+	print(mel)
+
 	# call swap function
 	swapMel(a, b, mel)
-		
-	print mel
+
+	print(mel)
 
 
 def hasjSort(mel, mir):
 	swapcount = 0
-	for i in range(len(mel)): 
-		for j in range(i, len(mel)):
-			if mel[i] > mel[j]:	
-				swapMel(i, j, mel)
-				print mel
-		swapcount += 1
-	print swapcount
+	mel_len = len(mel)
+	mir_len = len(mir)
+	if mel_len is not mir_len:
+		message = "error, lists are not the same length"
+		return message
+	else:
+		for i in range(mel_len):
+			if mir[i] is not mel[i]:
+				for j in range(i,mel_len):
+					if mir[i] is mel[j]:
+						mel = swapMel(i,j,mel)
+						swapcount += 1
+	print(mel)
+	print(swapcount)
+
+	return mel
 
 hasjSort(mel, mir)
-
-
-
 
 
