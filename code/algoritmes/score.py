111c97d469756a7144ff6d1c6108a0db60e2f49a
Pietje123 <36188449+Pietje123@users.noreply.github.com>
2018-05-15 13:59:29 +0200

    added scoreModifier

--- code/algoritmes/score.py
+++ code/algoritmes/score.py
@@ -8,3 +8,18 @@ def scoreNeighbours(mel):
             score += 1
         if abs(checkRight) == 1:
             score += 1
+    return score
+
+def scoreNeighboursModifier(mel):
+    score = 0
+    length = len(mel)
+    n = 0
+    for i in range(length - 1):
+        check= mel[i] - mel[i + 1]
+
+        if abs(check) == 1:
+            n += 1
+        else:
+            score += n*n
+            n= 0
+    return score
