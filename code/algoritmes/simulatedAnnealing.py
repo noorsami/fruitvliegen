548fade34a973dccf221a7e3069c57e2f90cb9dc
jurrians <jurrianschreuder@gmail.com>
2018-05-18 00:05:03 +0200

    simulatedAnnealing sneller gemaakt

--- code/algoritmes/simulatedAnnealing.py
+++ code/algoritmes/simulatedAnnealing.py
@@ -67,10 +67,12 @@ def simulatedAnnealing(mel, mir, failValue):
             scoreHistory.append(curScore)
             swaps += 1
 
-        if mutatedScore == curScore:
+        if mutatedScore == curScore or mutatedScore == (curScore - 1):
             failCount += 1
 
-            if failCount % failValue == 0:
+            check = failCount % failValue
+            marge = int(failValue * 0.001)
+            if  check in range(0, marge):
                 print("                                                                       Herberekening route.")
 
                 curMel = mutatedMel
@@ -93,9 +95,18 @@ def simulatedAnnealing(mel, mir, failValue):
     return history
 
 
-#mel = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9]
-#mir = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-#mir2 = [2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
 
-#simulatedAnnealing(mel, mir, 1000)
+
+
+
+
+
+
+mel = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9]
+mir = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+mir2 = [2, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+test1 = [1,2,3,5,4]
+test2 = [1,2,3,4,5]
+
+simulatedAnnealing(mel, mir, 1000)
