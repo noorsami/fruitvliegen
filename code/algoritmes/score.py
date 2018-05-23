15bf1de923fc471ac1cbcfa3cbcaed2ad8dbcd50
jurrians <jurrianschreuder@gmail.com>
2018-05-23 15:55:15 +0200

    UI main.py voor simAnn gefixt, tester.py toegevoegd

--- code/algoritmes/score.py
+++ code/algoritmes/score.py
@@ -1,26 +1,28 @@
-def scoreNeighbours(mel):
+class score:
 
-    score = 0
-    length = len(mel)
-    for i in range(1, length - 1):
-        checkLeft = mel[i] - mel[i - 1]
-        checkRight = mel[i] - mel[i + 1]
-        if abs(checkLeft) == 1:
-            score += 1
-        if abs(checkRight) == 1:
-            score += 1
-    return score
+    def scoreNeighbours(mel):
 
-def scoreNeighboursModifier(mel):
-    score = 0
-    length = len(mel)
-    n = 0
-    for i in range(length - 1):
-        check = mel[i] - mel[i + 1]
+        score = 0
+        length = len(mel)
+        for i in range(1, length - 1):
+            checkLeft = mel[i] - mel[i - 1]
+            checkRight = mel[i] - mel[i + 1]
+            if abs(checkLeft) == 1:
+                score += 1
+            if abs(checkRight) == 1:
+                score += 1
+        return score
 
-        if abs(check) == 1:
-            n += 1
-        else:
-            score += n*n
-            n = 0
-    return score
+    def scoreNeighboursModifier(mel):
+        score = 0
+        length = len(mel)
+        n = 0
+        for i in range(length - 1):
+            check = mel[i] - mel[i + 1]
+
+            if abs(check) == 1:
+                n += 1
+            else:
+                score += n*n
+                n = 0
+        return score
--- main.py
+++ main.py
@@ -10,10 +10,12 @@ sys.path.append(os.path.join(directory, "code", "algoritmes"))
 
 from datastructuur import data
 from helper import helper
+from score import score
 from pancakeSort import pancakeSort
 from randomSort import randomSort
 from population import populationBased
 from simulatedAnnealing import simulatedAnnealing
+from tester import tester
 
 print("\nWELCOME!\n\n When you want to run the pancake sorting algorithm type in: 1 \n\n When you want to run the population based algorithm type in: 2 \n\n When you want to run the simulated annealing algorithm type in: 3\n")
 
@@ -28,8 +30,44 @@ elif algorithm == '2':
     print(populationBased)
 
 elif algorithm == '3':
-    simulated = simulatedAnnealing(data.mel, data.mir, 1000)
-    print(simulated)
+    
+    default = input("\nIf you want to run the algorithm once with the default parameters, type 'd'. For custom parameters, type 'c'. \
+    \nFor running the algorithm (with default parameters) on a test-set with 100 random genomes of length 25, type 't'  \n\n")
 
-else:
-    print("Error, unknown input")
+    if default == 'd':
+        print(simulatedAnnealing(data.mel, data.mir, 1000, score.scoreNeighbours))
+
+    elif default == 'c':
+
+        print("\nDefine the score-function you would like to use.\n\nFor scoreNeighbours, type: 1\nFor scoreNeighboursModifier, type: 2\n")
+        scoreF = input("Type in which score-funtion that you would like the algorithm to use: ")
+
+        if scoreF == '1':
+            print("\nDefine the interval with which the algorithm allows lesser mutations to pass.\n\nFor an interval of 1000, type: s\nFor an interval of 10.000, type: m\nFor an interval of 100.000, type l\n")
+            failV = input("Type in which interval-size you would like the algorithm to use: ")
+
+            if failV == 's':
+                print(simulatedAnnealing(data.mel, data.mir, 1000, score.scoreNeighbours))
+            if failV == 'm':
+                print(simulatedAnnealing(data.mel, data.mir, 10000, score.scoreNeighbours))
+            if failV == 'l':
+                print(simulatedAnnealing(data.mel, data.mir, 100000, score.scoreNeighbours))
+
+        elif scoreF == '2':
+            print("\nDefine the interval with which the algorithm allows lesser mutations to pass.\n\nFor an interval of 1000, type: s\nFor an interval of 10.000, type: m\nFor an interval of 100.000, type l\n")
+            failV = input("Type in which interval-size you would like the algorithm to use: ")
+
+            if failV == 's':
+                print(simulatedAnnealing(data.mel, data.mir, 1000, score.scoreNeighboursModifier))
+            if failV == 'm':
+                print(simulatedAnnealing(data.mel, data.mir, 10000, score.scoreNeighboursModifier))
+            if failV == 'l':
+                print(simulatedAnnealing(data.mel, data.mir, 100000, score.scoreNeighboursModifier))
+        else:
+            print("Error, unknown input")
+
+    if default == 't':
+        tester.simulatedAnnealing()
+
+    else:
+        print("Error, unknown input")
