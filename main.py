53e8ead71673b1bf3d7c3f7fdba7ad185e7c4575
jurrians <jurrianschreuder@gmail.com>
2018-05-23 17:01:25 +0200

    UI verder aangepast + kleine aanpassingen aan pancakeSort

--- main.py
+++ main.py
@@ -15,24 +15,57 @@ from pancakeSort import pancakeSort
 from randomSort import randomSort
 from population import populationBased
 from simulatedAnnealing import simulatedAnnealing
+from steepestDescendValleyAbseiler import steepestDescendValleyAbseiler
 from tester import tester
 
-print("\nWELCOME!\n\n When you want to run the pancake sorting algorithm type in: 1 \n\n When you want to run the population based algorithm type in: 2 \n\n When you want to run the simulated annealing algorithm type in: 3\n")
+print("\nWELCOME!\n\n When you want to run the pancake sorting algorithm type in: 1 \
+                 \n\n When you want to run the population based algorithm type in: 2 \
+                 \n\n When you want to run the simulated annealing algorithm type in: 3 \
+                 \n\n When you want to run the Steepest Descend Valley Abseiler algorithm, type in: 4\n")
 
 algorithm = input("Type in the algorithm that you want to run: \n\n")
 
+''' PANCAKESORT '''
 if algorithm == '1':
-    pancake = pancakeSort(data.mel, data.mir)
-    print(pancake)
+    default = input("\n If you want to run the algorithm with default parameters, type 'd'.\n\n \
+                        For running the algorithm on a test-set with 100 random genomes of length 25, type 't' \n\n")
 
-elif algorithm == '2':
-    populationBased = populationBased(300, data.mel, data.mir)
-    print(populationBased)
+    if default == 'd':
+        print(pancakeSort(data.mel, data.mir))
+    if default == 't':
+        tester.pancakeSort()
 
-elif algorithm == '3':
-    
-    default = input("\nIf you want to run the algorithm once with the default parameters, type 'd'. For custom parameters, type 'c'. \
-    \nFor running the algorithm (with default parameters) on a test-set with 100 random genomes of length 25, type 't'  \n\n")
+''' POPULATION BASED '''
+if algorithm == '2':
+    default = input("\n If you want to run the algorithm once with the default parameters, type 'd'.\n\n \
+                        For custom parameters, type 'c'. \n\n \
+                        For running the algorithm (with default parameters) on a test-set with 100 random genomes of length 25, type 't'  \n\n")
+
+    if default == 'd':
+        print(populationBased(300, data.mel, data.mir))
+
+    if default == 'c':
+        print("\nDefine the population size you would like the algorithm to use.\n\n \
+                For a population size of 50, type 'xs'.\n\n \
+                For a population size of 150, type 's'\n\n \
+                For a population size of 300, type 'm'.\n\n \
+                For a population size of 500, type 'l'\n\n \
+                For a population size of 1000, type 'xl'\n\n)
+        print("TODO")
+
+    if default == 't':
+        tester.populationBased()
+    #
+    # else:
+    #     print("Error, unknown input")
+
+
+''' SIMULATED ANNEALING '''
+if algorithm == '3':
+
+    default = input("\n If you want to run the algorithm once with the default parameters, type 'd'.\n\n \
+                        For custom parameters, type 'c'. \n\n \
+                        For running the algorithm (with default parameters) on a test-set with 100 random genomes of length 25, type 't'  \n\n")
 
     if default == 'd':
         print(simulatedAnnealing(data.mel, data.mir, 1000, score.scoreNeighbours))
@@ -66,8 +99,15 @@ elif algorithm == '3':
         else:
             print("Error, unknown input")
 
-    if default == 't':
+    elif default == 't':
         tester.simulatedAnnealing()
 
-    else:
-        print("Error, unknown input")
+    # else:
+    #     print("Error, unknown input")
+
+''' STEEPEST DESCEND VALLEY ABSEILER '''
+if algorithm == '4':
+    print(steepestDescendValleyAbseiler(data.mir, data.mel))
+#
+# else:
+#     print("Error, unknown input")
