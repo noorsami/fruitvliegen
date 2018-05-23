c9c3a0d25c5f043309caeb734724fd4ee859ac1c
Pietje123 <36188449+Pietje123@users.noreply.github.com>
2018-05-23 11:08:30 +0200

    update

--- testAlwan.py
+++ testAlwan.py
@@ -16,45 +16,20 @@ sys.path.append(os.path.join(directory, "code", "classes"))
 sys.path.append(os.path.join(directory, "code", "algoritmes"))
 
 from helper import helper
-from breadthFirst import breadthFirst
 
 
-def steepestAscendHillClimber(mir,mel):
-    mir = tuple(mir)
-    melSet = set(tuple(copy.copy(mel)))
-    melList = [tuple(copy.copy(mel))]
-    swaps = 0
-    count = 0
-    melListHistory = []
-    q = queue.Queue()
-    breakpoints = []
-    melLen = len(helper.makeSequence(mel))
-    while mir not in melSet:
+def branchAndBound(mir,mel):
 
-        melListHistory.append(len(melList))
-        breakpoints.append(melLen)
+    melSeq = helper.makeSequence(mel)
+    breakpoints = [len(melSeq)]
+    allSwaps = helper.swapAllSequence(melSeq)
+    print(allSwaps[1])
+    print(melSeq)
 
 
-        q.put(copy.copy(melList))
-        melList = []
-        while not q.empty():
-            allGens = q.get()
-            swaps+=1
-            for gen in allGens:
-                seq = helper.makeSequence(gen)
-                allSwaps = helper.swapAllSequence(seq)
 
-                for swap in allSwaps:
-                	if len(swap) <= melLen:
-                		melLen = len(swap)
-                		swap = helper.makeList(swap)
-                		if swap not in melSet:
-                			melSet.add(swap)
-                			melList.append(swap)
-                	else:
-                		count+=1
-        			
-    return melListHistory, swaps, count
+
+
 
 # length = 25
 # gen1 = [*range(1,length + 1)]
@@ -62,8 +37,7 @@ def steepestAscendHillClimber(mir,mel):
 # rm.shuffle(gen2)
 mel = [23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20, 5, 8, 18, 12, 13, 14, 15, 16, 17, 21, 3, 4, 9]
 mir = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-print(steepestAscendHillClimber(mir,mel))
-
+branchAndBound(mir,mel)
 # data1  = [1, 235, 8784, 2740, 39997, 57155, 42241, 343473, 27862, 79871, 153052, 21481,5740, 9899, 3169, 1851, 525]
 # data2 = [22, 22, 21, 19, 18, 16, 15, 14, 12, 11, 10, 8, 7, 6, 5, 4, 3]
 # length = 25
